b2d9935405d46f41375ee9ddc3c8d601d05c5078
cowpatchmedia <cowpatchmedia@gmail.com>
2025-11-17 20:52:45 -0800

    move background picture from js to css.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,5 +1,2 @@
 export const greeting = "Hello World!";
 
-import restaurantImage from "./../media/restaurant.jpg";
-
-document.getElementById("left-panel").style.backgroundImage = `url(${restaurantImage})`;
