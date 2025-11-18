4d00e77dda9fbb73dfd1b4c5cf4199cd1ecbce5d
cowpatchmedia <cowpatchmedia@gmail.com>
2025-11-17 19:05:57 -0800

    initial commit for restaurant webpage.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {greeting} from "./scripts.js";
+import "./stylesheets/styles.css";
+import "./stylesheets/modern-normalize.css";
+
+console.log(greeting);
---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+export const greeting = "Hello World!";
+
+import restaurantImage from "./media/restaurant.jpg";
+
+const image = document.createElement("img");
+image.src = restaurantImageImage;
+
+document.body.appendChild(image);
