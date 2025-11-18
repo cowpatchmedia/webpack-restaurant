1ba5e2ce4fbc67a5a958fa51ef796897090db62c
cowpatchmedia <cowpatchmedia@gmail.com>
2025-11-17 21:02:21 -0800

    make home page appear via js instead of html.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,2 +1,19 @@
-export const greeting = "Hello World!";
+export const greeting = "Hello Home!";
 
+export default function loadHome() {
+    const container = document.createElement("div");
+
+    const title = document.createElement("div");
+    title.id = "content-title";
+    title.textContent = "welcome to haley's cafe";
+
+    const body = document.createElement("div");
+    body.id = "content-body";
+    body.textContent =
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id ducimus, quasi assumenda quas aliquam quis praesentium iure doloribus.";
+
+    container.appendChild(title);
+    container.appendChild(body);
+
+    return container;
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,31 @@
 import {greeting} from "./home.js";
+import loadHome from "./home.js";
+
 import "./../stylesheets/styles.css";
 import "./../stylesheets/modern-normalize.css";
 
 console.log(greeting);
+
+const content = document.getElementById("content");
+
+// render old content
+function render(moduleFn) {
+    content.innerHTML = "";       // wipe old content
+    content.appendChild(moduleFn());
+}
+
+// home page on load
+render(loadHome);
+
+// Add button listeners
+document.getElementById("home-btn").addEventListener("click", () => {
+    render(loadHome);
+});
+
+document.getElementById("menu-btn").addEventListener("click", () => {
+    render(loadHome);
+});
+
+document.getElementById("about-btn").addEventListener("click", () => {
+    render(loadHome);
+});
